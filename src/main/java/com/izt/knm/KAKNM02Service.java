04c9edb9f6df4a03ac287ab9fd1cca79e612efde
yeeun0514 <yeeun.g.sin@gmail.com>
2020-07-28 13:00:24 +0900

    knm02sql 수정, 메일보내기 완료

--- src/main/java/com/izt/knm/KAKNM02Service.java
+++ src/main/java/com/izt/knm/KAKNM02Service.java
@@ -17,12 +17,17 @@ import com.izt.common.CreateTbIdService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.izt.common.mail.MailService;
+import com.izt.common.mail.EmailMessage;
+
 @Service
 public class KAKNM02Service {
     @Autowired
 	private KAKNM02DAO kAKNM02DAO;
 	@Autowired
-    private CreateTbIdService createTbIdService;
+	private CreateTbIdService createTbIdService;
+	@Autowired
+    private MailService mailService;
 	
 	// 내가 답변한 리스트
 	public List<KAKNM02VO> getList(String title, String status, String user) {
@@ -52,6 +57,12 @@ public class KAKNM02Service {
 		System.out.println("KAKNM02 << Service >> postWrite Called...!!");
 		// 태그를 # 단위로 분리해서 배열로 저장
 		String [] tag = tag(kAKNM02VO.getContent_t());
+		// email 발송 변수
+		String user_id ="";
+        String setSubject="";
+		String setBody="";
+		String title =kAKNM02VO.getTitle();
+		user_id = kAKNM02VO.getReg_userid_tq();
 		
 		// 답변작성이라면
 		if(kAKNM02VO.getDo_type().equals("W")){
@@ -60,6 +71,13 @@ public class KAKNM02Service {
 			kAKNM02VO.setAnswer_id(createTbIdService.createPkId("AN"));
 			// answer insert
 			kAKNM02DAO.postWrite(kAKNM02VO);
+			// 질문자에게 이메일 발송
+			System.out.println("이메일 발송 !!! " + user_id);
+			setSubject = "[솔루션지식자산화시스템] 회원님의 질문에 답변이 등록되었습니다.";
+			setBody = "안녕하세요. 솔루션지식자산화시스템입니다. <br />" 
+						+ "회원님의 \"" + title + "\" 질문에 답변이 등록되었습니다. <br />"
+						+ "확인부탁드립니다. 감사합니다.";
+			sendMail(setSubject, setBody, user_id);
 		}
 		// 수정이라면 (answer id 생성할 필요 없음)
 		else if(kAKNM02VO.getDo_type().equals("M")){
@@ -68,6 +86,11 @@ public class KAKNM02Service {
 			kAKNM02DAO.delTag(kAKNM02VO);
 			// 수정하기
 			kAKNM02DAO.modify(kAKNM02VO);
+			// setSubject = "[솔루션지식자산화시스템] 회원님의 질문의 답변이 수정되었습니다.";
+			// setBody = "안녕하세요. 솔루션지식자산화시스템입니다. <br />" 
+			// 			+ "회원님의 \"" + title + "\" 질문의 답변이 수정되었습니다. <br />"
+			// 			+ "확인부탁드립니다. 감사합니다.";
+			// sendMail(setSubject, setBody, user_id);
 		}
 
 		// tag 갯수만큼 아이디 생성해서 넣어주기
@@ -92,6 +115,24 @@ public class KAKNM02Service {
 		String [] arr = t2.split("#");
 		return arr;
 	}
+
+	// 메일전송
+    public void sendMail(String setSubject, String setBody, String user_id){
+        try {
+         // 메일 전송
+         EmailMessage emailMessage = new EmailMessage();
+         emailMessage.setBody(setBody);
+         
+         emailMessage.setSubject(setSubject);
+         emailMessage.setTo_address(user_id); //user 이메일 user_id로 바꿔야함
+
+         mailService.sendmail(emailMessage); //메일 발송  
+            
+        } catch (Exception e) {
+            //TODO: handle exception
+          e.printStackTrace();
+        }
+    }
 	
     
 }
